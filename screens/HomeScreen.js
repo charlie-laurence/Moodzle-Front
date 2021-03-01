7d20a52c506c9f13abe919599a0839ab1ce1e572
Elodie Saubatte <elodie-saubatte@hotmail.fr>
2021-03-01 23:03:03 +0100

    mise en place de l'enregistrement du username en local

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,88 +1,88 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { StyleSheet, ImageBackground, Text, View } from 'react-native';
+import { StyleSheet, ImageBackground, Text, View } from "react-native";
 
-import {Button, Input} from 'react-native-elements';
+import { Button, Input } from "react-native-elements";
 
-import { connect } from 'react-redux';
-
+import { connect } from "react-redux";
 
 function HomeScreen(props) {
+  const [pseudo, setPseudo] = useState("");
+  console.log('pseudo', pseudo);
+  const [pseudoSubmited, setPseudoSubmited] = useState(false);
 
-const [pseudo, setPseudo] = useState('');
-console.log(pseudo);
-
-useEffect(() => {
-  var pseudoRegistered = async function() {
-  await AsyncStorage.getItem("pseudo", (err, value) => {
-    setPseudo(value);
-    console.log('value', value)
-  });
-}
-  pseudoRegistered()
- }, []);
-
- if (pseudo !== null) {
-   var homeScreenContent =
-   <View>
-     <Text>Bienvenue {pseudo} !</Text>
-     </View>
- } else {
-   var homeScreenContent =
-   <View>
-            <Input
-            containerStyle = {{marginBottom: 25, width: '70%'}}
-            inputStyle={{marginLeft: 10}}
-            placeholder="Nom d'utilisateur"
-            onChangeText={(e) => setPseudo(e)}
-            />          
-        <Button
-            title="C'est parti !"
-            type="solid"
-            buttonStyle = {{backgroundColor: "#009788"}}
-
-            onPress={() => { props.onSubmitPseudo(pseudo); 
-                            AsyncStorage.setItem("pseudo", pseudo);
-                            props.navigation.navigate('BottomNavigator', { screen: 'Mood' });
-        }}
-        />
-        </View>
-
- }
-
-    return ( 
-      <ImageBackground source={require('../assets/MoodzSignUp.png')} style={styles.container}>
-        <View>
-      {homeScreenContent} 
-      </View>
-      </ImageBackground>
-);
-    }
-    
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-    });
-
-
-
-    function mapDispatchToProps(dispatch) {
-      return {
-        onSubmitPseudo: function(pseudo) {
-           dispatch( {type: 'savePseudo', pseudo: pseudo })
+  useEffect(() => {
+      AsyncStorage.getItem("pseudo", (err, value) => {
+        if (value) {
+        setPseudo(value);
+        setPseudoSubmited(true);
         }
-      }
-     }
+      });
+  }, []);
 
-     export default connect(
-      null,
-      mapDispatchToProps
-     )(HomeScreen);
 
-    
+var inputUsername;
+if (!pseudoSubmited) {
+  inputUsername = <Input
+  containerStyle={{ marginBottom: 25, width: '70%' }}
+  inputStyle={{ marginLeft: 10 }}
+  placeholder="Nom d'utilisateur"
+  onChangeText={(content) => setPseudo(content)}
+  />
+  } else {
+    inputUsername = <Text style={styles.paragraph}>Bienvenue {pseudo} !</Text>
+  }
+
+  return (
+    <ImageBackground
+      source={require("../assets/MoodzSignUp.png")}
+      style={styles.container}
+    >
+      {inputUsername}
+
+      <Button
+          title="C'est parti !"
+          type="solid"
+          buttonStyle={{ backgroundColor: "#009788" }}
+          onPress={() => {
+            setPseudoSubmited(true);
+            props.onSubmitPseudo(pseudo);
+            AsyncStorage.setItem("pseudo", pseudo);
+            props.navigation.navigate("BottomNavigator", { screen: "Mood" });
+          }}
+        />
+
+    </ImageBackground>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paragraph: {
+    fontWeight: 'normal',
+    fontSize: 25,
+    textAlign: 'center',
+    color: 'black', 
+    marginBottom: 20,
+},
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onSubmitPseudo: function (pseudo) {
+      dispatch({ type: "savePseudo", pseudo: pseudo });
+    },
+  };
+}
+
+export default connect(
+  null, 
+  mapDispatchToProps
+  )(HomeScreen);
