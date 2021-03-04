74935f9b367f788542b0d874fb97b847d328bbfe
Charlotte <charlotte.laurence@hotmail.fr>
2021-03-04 18:16:15 +0100

    dashboard ok

--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,8 +1,10 @@
 import React, {useState, useEffect} from "react";
 import { StyleSheet, Text, View, Button, ScrollView, Flat } from "react-native";
 import { Card } from "react-native-elements";
+import { FontAwesome5 } from "@expo/vector-icons";
 import { connect } from "react-redux";
 import { _IP_OLIV , _IP_CAPSULE } from "../statics/ip";
+import { moodData } from "../statics/icon";
 
 function HistoryScreen({ navigation, updateMood }) {
   const [historyFromBack , setHistoryFromBack] = useState([]);
@@ -18,24 +20,40 @@ function HistoryScreen({ navigation, updateMood }) {
   }, []);
 
   /* Exploiter l'historique de l'utilisateur pour afficher ses infos : */
-  var moodList = historyFromBack.map((item, i) => {
+    var moodList = historyFromBack.map((item, i) => {
+
+      var activityList = [];
+      for (var j=0 ; j<item.activity.length ; j++) {
+        activityList.push(item.activity[j].name);
+      }
+
     return (
     <Card
       key={i}
       bottomDivider
+      wrapperStyle={styles.cardWrapper}
+      containerStyle={styles.cardContainer}
     >
-      <Card.Title>
-        <Text>{item.mood_score}</Text>
-      </Card.Title>
-      <Card.Title>
+      <FontAwesome5
+        style={{margin:15}}
+        key={i}
+        name={moodData[item.mood_score-1].icon}
+        size={70}
+        color={moodData[item.mood_score-1].color}
+        score={item.mood_score}
+      />
+      <Card.Title style={{color:"#57706D"}}>
         <Text>{item.date}</Text>
+      </Card.Title>
+      <Card.Title style={{color:"#57706D"}}>
+        <Text>{activityList.join(", ")}</Text>
       </Card.Title>
     </Card>
     )
   })
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.paragraph}>Tableau de Bord</Text>
       <Button
         title="RETOUR SUR AJOUT MOOD"
@@ -45,25 +63,47 @@ function HistoryScreen({ navigation, updateMood }) {
           updateMood();
         }}
       />
+      <View
+      // style={{paddingBottom:200}}
+      >
       <ScrollView>
         {moodList}
       </ScrollView>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor:"#F0FFF9",
+  },
+  cardWrapper: {
+    display:"flex",
+    justifyContent:"space-around",
+    alignItems:"center",
+    height:150,
+  },
+  cardContainer: {
+    padding:0,
+    backgroundColor: "white",
+    shadowColor: "#F0FFF9",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    elevation: 10,
+    borderColor: "white",
+    borderRadius: 25,
   },
   paragraph: {
     fontWeight: "bold",
     fontSize: 30,
     textAlign: "center",
     color: "#009788",
-    marginTop: 70,
+    marginTop: 50,
   },
 });
 
---
+++ statics/icon.js
@@ -0,0 +1,23 @@
+const moodData = [
+    {
+    icon: "angry",
+    color: "#CD6133",
+    },
+    {
+    icon: "sad-cry",
+    color: "#F0A07E",
+    },
+    {
+    icon: "meh",
+    color: "#F0D231",
+    },
+    {
+    icon: "grin-squint",
+    color: "#44B79D",
+    },
+    {
+    icon: "smile-beam",
+    color: "#54857F",
+    },
+];
+export { moodData };
