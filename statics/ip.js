08631a17d454f168da78dd046a41fb4ce76aad9e
RachelRamadour <rramadour@gmail.com>
2021-03-05 16:06:41 +0100

    recup master

--- statics/ip.js
+++ statics/ip.js
@@ -1,5 +1,5 @@
 const _IP_OLIV = "192.168.1.85";
-const _IP_CAPSULE = "172.17.1.87";
+const _IP_CAPSULE = "172.17.1.159";
 // const _IP_CAPSULE = "172.17.1.144";
 
 const proxy = `http://${_IP_CAPSULE}:3000`;
