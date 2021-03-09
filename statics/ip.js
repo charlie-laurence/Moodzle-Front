88dbb461b8aaa0296831a9b174db90c2e4bbcd89
TY Lau <lautsyyeung@gmail.com>
2021-03-09 14:34:18 +0100

    filter + linechart month

--- statics/ip.js
+++ statics/ip.js
@@ -2,10 +2,9 @@ const _IP_OLIV = "192.168.1.85";
 const _IP_CAPSULE = "172.17.1.159";
 const _IP_RACH = "192.168.0.16";
 const _IP_TSY = "172.17.1.144";
-const _IP_TSY2 = "192.168.42.60";
 const _IP_CHARLIE = "192.168.1.55";
 const _IP_ELO = "172.17.1.15";
 
-const proxy = `http://${_IP_TSY2}:3000`;
+const proxy = `http://${_IP_OLIV}:3000`;
 
 export { proxy };
