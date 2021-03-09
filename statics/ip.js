c46747995851b1e0d055f5a9ccc4f12d52b01a25
Charlotte <charlotte.laurence@hotmail.fr>
2021-03-09 11:14:53 +0100

    bug sign up fixé

--- statics/ip.js
+++ statics/ip.js
@@ -4,6 +4,6 @@ const _IP_CHARLIE = "192.168.1.55";
 const _IP_ELO = "172.17.1.15";
 const _IP_CAPSULE = "172.17.1.158";
 
-const proxy = `http://${_IP_OLIV}:3000`;
+const proxy = `http://${_IP_CAPSULE}:3000`;
 
 export { proxy };
