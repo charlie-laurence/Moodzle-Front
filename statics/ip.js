2dd01d3307cbf5a246558827ddccf2aaadfbccdb
olivblad <obladier@gmail.com>
2021-03-11 12:33:22 +0100

    ip capsule

--- statics/ip.js
+++ statics/ip.js
@@ -1,11 +1,11 @@
 const _IP_OLIV = "192.168.1.85";
-const _IP_CAPSULE = "172.17.1.158";
+const _IP_CAPSULE = "172.17.1.87";
 const _IP_RACH = "192.168.0.16";
 const _IP_TSY = "192.168.42.60";
 const _IP_CHARLIE = "192.168.1.55";
 const _IP_ELO = "172.17.1.15";
 const _IP_ELO2 = "192.168.1.62";
 
-const proxy = `http://${_IP_OLIV}:3000`;
+const proxy = `http://${_IP_CAPSULE}:3000`;
 
 export { proxy };
